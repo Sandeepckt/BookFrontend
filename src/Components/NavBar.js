0c3883fcc873c730f78f7a62f84fb4afc3f2d52c
ChaurasiaSwati1 <134093144+ChaurasiaSwati1@users.noreply.github.com>
2023-08-22 01:22:41 +0530

    nav

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -14,16 +14,64 @@ import {
   NavbarText,
 } from 'reactstrap';
 
+import logo from "../assets/img/logo_book.svg"
 function NavBar(args) {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <div>
-      <Navbar {...args}>
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
+    <>
+
+    <Navbar {...args}>
+          
+        <Nav className="w-100 d-md-flex ml-auto justify-content-between flex-row d-none">
+          <div className='d-inline-flex align-items-center'>
+            <NavbarBrand href="/"><img src={logo}/></NavbarBrand>
+        
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                GitHub
+              </NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            </div>
+          <div className='d-inline-flex align-items-center ml-auto'>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                GitHub
+              </NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            </div>
+          </Nav>
+        <NavbarToggler onClick={toggle} className='d-md-none d-inline-block'/>
         <Collapse isOpen={isOpen} navbar>
           <Nav className="me-auto" navbar>
             <NavItem>
@@ -49,7 +97,7 @@ function NavBar(args) {
           <NavbarText>Simple Text</NavbarText>
         </Collapse>
       </Navbar>
-    </div>
+    </>
   );
 }
 
