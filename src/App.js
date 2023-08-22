d0180068e64a0520847564a05b9eda5d7808b310
Sandeep Chaurasia <sandeepchaurasia000@gmail.com>
2023-08-22 21:01:11 +0530

    bookcart

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
+import React from 'react';
 
 import './App.css';
-import Home from './Components/Home';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { BrowserRouter , Routes, Route } from 'react-router-dom'
+import SignInPage from './Components/SignInPage';
+
+import SignUpPage from './Components/SignUpPage';
+import Main from './Components/Main';
+import Cart from './Components/Cart';
 
 
 function App() {
   return (
-    <>
-      <Home/> 
-    </>
+    <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+      <Route path='/' element={<Main />}> 
+        
+          <Route path='login' element={<SignInPage/>}/>
+          <Route path='signup' element={<SignUpPage />}/>
+          <Route path='cart' element={<Cart/>}/>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>  
+  </React.StrictMode>
   );
 }
 
